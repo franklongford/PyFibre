2506bb2a14b9ed9102d88616955b4e7fa0b75474
franklongford <franklongford@gmail.com>
2019-12-20 21:37:12 +0000

    DEV: included save and load routines for lists of BaseGraphSegment subclasses

--- pyfibre/io/object_io.py
+++ pyfibre/io/object_io.py
@@ -2,42 +2,89 @@ import numpy as np
 from networkx.readwrite import node_link_graph
 
 from pyfibre.model.objects.base_graph_segment import BaseGraphSegment
+from pyfibre.model.objects.fibre_network import FibreNetwork
 from pyfibre.utilities import (
     save_pickle, load_pickle, save_json, load_json)
 
 
-def save_base_graph_segment(object, file_name, file_type=None):
+def get_networkx_graph(data):
+    """Transform JSON serialised data into a
+    networkx Graph object"""
 
-    data = object.__getstate__()
+    for coord in data['nodes']:
+        coord['xy'] = np.array(coord['xy'])
 
-    try:
-        save_json(data, f"{file_name}.json")
-    except IOError as e:
-        raise IOError(
-            f"Cannot save to file {file_name}.json"
-        ) from e
+    return node_link_graph(data)
 
 
-def load_base_graph_segment(file_name, file_type=None):
+def save_base_graph_segment(graph_segment, file_name, file_type=None):
+
+    if file_type is not None:
+        file_name = '_'.join([file_name, file_type])
+
+    data = graph_segment.__getstate__()
+
+    save_json(data, file_name)
+
+
+def load_base_graph_segment(file_name, file_type=None, klass=BaseGraphSegment):
     """Loads pickled image objects"""
 
     if file_type is not None:
         file_name = '_'.join([file_name, file_type])
 
-    try:
-        data = load_json(f"{file_name}.json")
+    data = load_json(file_name)
 
-    except IOError as e:
-        raise IOError(
-            f"Cannot read file {file_name}.json"
-        ) from e
+    data['graph'] = get_networkx_graph(data['graph'])
 
-    for coord in data['graph']['nodes']:
-        coord['xy'] = np.array(coord['xy'])
+    return klass(**data)
 
-    data['graph'] = node_link_graph(data['graph'])
 
-    return BaseGraphSegment(**data)
+def save_base_graph_segments(graph_segments, file_name, file_type=None):
+
+    if file_type is not None:
+        file_name = '_'.join([file_name, file_type])
+    else:
+        file_type = 'graph_segment'
+
+    data = {
+        file_type : [
+            graph_segment.__getstate__()
+            for graph_segment in graph_segments]
+    }
+
+    save_json(data, file_name)
+
+
+def load_base_graph_segments(file_name, file_type=None, klass=BaseGraphSegment):
+
+    if file_type is not None:
+        file_name = '_'.join([file_name, file_type])
+    else:
+        file_type = 'graph_segment'
+
+    data = load_json(file_name)
+
+    for graph_segment in data[file_type]:
+        graph_segment['graph'] = get_networkx_graph(
+            graph_segment['graph'])
+
+    graph_segments = [
+        klass(**kwargs) for kwargs in data[file_type]
+    ]
+
+    return graph_segments
+
+
+def save_fibre_networks(fibre_networks, file_name):
+    """Save a list of FibreNetwork instances"""
+    save_base_graph_segments(fibre_networks, file_name, 'fibre_networks')
+
+
+def load_fibre_networks(file_name):
+    """Load a list of FibreNetwork instances"""
+    return load_base_graph_segments(
+        file_name, 'fibre_networks', FibreNetwork)
 
 
 def save_objects(objects, file_name, file_type=None):
@@ -60,7 +107,6 @@ def load_objects(file_name, file_type=None):
     if file_type is not None:
         file_name = '_'.join([file_name, file_type])
 
-    print(file_name)
     try:
         objects = load_pickle(f"{file_name}.pkl")
         return objects
--- pyfibre/io/tests/test_object_io.py
+++ pyfibre/io/tests/test_object_io.py
@@ -1,8 +1,13 @@
 from unittest import TestCase
 import os
 
+import numpy as np
+import networkx as nx
+
 from pyfibre.io.object_io import (
-    save_base_graph_segment, load_base_graph_segment)
+    get_networkx_graph,
+    save_base_graph_segment, load_base_graph_segment,
+    save_base_graph_segments, load_base_graph_segments)
 from pyfibre.model.objects.base_graph_segment import (
     BaseGraphSegment
 )
@@ -15,6 +20,25 @@ class TestObjectIO(TestCase):
 
         self.graph = generate_probe_graph()
         self.graph_segment = BaseGraphSegment(graph=self.graph)
+
+    def test_get_networkx_graph(self):
+
+        data = {
+            'directed': False,
+            'graph': {},
+            'links': [],
+            'multigraph': False,
+            'nodes': [{'xy': [0, 0], 'id': 2},
+                      {'xy': [1, 1], 'id': 3},
+                      {'xy': [2, 2], 'id': 4},
+                      {'xy': [2, 3], 'id': 5}]
+        }
+
+        graph = get_networkx_graph(data)
+
+        self.assertIsInstance(graph, nx.Graph)
+        self.assertEqual(4, graph.number_of_nodes())
+        self.assertIsInstance(graph.nodes[2]['xy'], np.ndarray)
 
     def test_save_load_base_graph_segment(self):
 
@@ -33,6 +57,32 @@ class TestObjectIO(TestCase):
             self.assertEqual(
                 self.graph.number_of_edges(),
                 test_segment.graph.number_of_edges()
+            )
+
+        finally:
+            if os.path.exists('test_json.json'):
+                os.remove('test_json.json')
+
+    def test_save_load_base_graph_segments(self):
+
+        try:
+            save_base_graph_segments(
+                [self.graph_segment, self.graph_segment],
+                'test_json')
+            self.assertTrue(os.path.exists('test_json.json'))
+
+            test_segments = load_base_graph_segments('test_json')
+
+            self.assertEqual(2, len(test_segments))
+            self.assertIsInstance(
+                test_segments[0], BaseGraphSegment)
+            self.assertEqual(
+                self.graph.number_of_nodes(),
+                test_segments[0].graph.number_of_nodes()
+            )
+            self.assertEqual(
+                self.graph.number_of_edges(),
+                test_segments[0].graph.number_of_edges()
             )
 
         finally:
