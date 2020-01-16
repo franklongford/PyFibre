aaf70bab3997ae4393f69e029355b85c8b5d4d1b
franklongford <franklongford@gmail.com>
2020-01-16 00:17:22 +0000

    CLN: refactored save_segments and load_segments routines

--- pyfibre/io/segment_io.py
+++ pyfibre/io/segment_io.py
@@ -1,37 +1,38 @@
 import numpy as np
 
-from pyfibre.model.tools.convertors import segments_to_binary, binary_to_segments
+from pyfibre.model.tools.convertors import (
+    segments_to_stack, stack_to_segments)
 
 
-def save_segment(segments, file_name, shape, file_type=None):
+def save_segments(segments, file_name, shape, file_type=None):
     """Saves scikit image regions as pickled file"""
 
-    binary = segments_to_binary(segments, shape)
+    stack = segments_to_stack(segments, shape)
 
     if file_type is not None:
         file_name = '_'.join([file_name, file_type])
 
     try:
-        np.save(f"{file_name}.npy", binary)
+        np.save(f"{file_name}.npy", stack)
     except IOError as e:
         raise IOError(
             f"Cannot save to file {file_name}"
         ) from e
 
 
-def load_segment(file_name, file_type=None, image=None):
+def load_segments(file_name, file_type=None, image=None):
     """Loads pickled scikit image regions"""
 
     if file_type is not None:
         file_name = '_'.join([file_name, file_type])
 
     try:
-        binary = np.load(f"{file_name}.npy")
+        stack = np.load(f"{file_name}.npy")
     except IOError as e:
         raise IOError(
             f"Cannot read file {file_name}.npy"
         ) from e
 
-    segments = binary_to_segments(binary, intensity_image=image)
+    segments = stack_to_segments(stack, intensity_image=image)
 
     return segments
