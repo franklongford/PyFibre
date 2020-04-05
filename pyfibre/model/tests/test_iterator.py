3eb9ec5e40296dfeb8fa69cd6ebac5b8f4b8c059
franklongford <franklongford@gmail.com>
2020-04-05 23:26:36 +0100

    DEV: updated iterator routine with unit tests

---
+++ pyfibre/model/tests/test_iterator.py
@@ -0,0 +1,31 @@
+from unittest import TestCase
+
+from pyfibre.model.iterator import assign_images
+
+
+class TestIterator(TestCase):
+
+    def setUp(self):
+
+        self.image_dictionary = {
+            'SHG-PL-Trans': '/directory/prefix-pl-shg-test.tif',
+            'PL-Trans': '/directory/prefix-pl-test.tif',
+            'SHG': '/directory/prefix-shg-test.tif'}
+
+    def test_assign_images(self):
+
+        filenames = assign_images(self.image_dictionary)
+        self.assertEqual(1, len(filenames))
+        self.assertEqual(
+            '/directory/prefix-pl-shg-test.tif',
+            filenames[0])
+
+        self.image_dictionary.pop('SHG-PL-Trans')
+        filenames = assign_images(self.image_dictionary)
+        self.assertEqual(2, len(filenames))
+        self.assertEqual(
+            '/directory/prefix-shg-test.tif',
+            filenames[0])
+        self.assertEqual(
+            '/directory/prefix-pl-test.tif',
+            filenames[1])
