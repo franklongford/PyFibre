be9fd47067bf6aaa1be4c467ad2dffce1a31697f
franklongford <franklongford@gmail.com>
2020-04-06 00:22:01 +0100

    STY: flake8 fix

--- pyfibre/api.py
+++ pyfibre/api.py
@@ -1,4 +1,4 @@
-from pyfibre.model.objects.fibre import Fibre
+from pyfibre.model.objects.fibre import Fibre # noqa
 from pyfibre.model.objects.segments import CellSegment # noqa
 from pyfibre.model.objects.fibre_network import FibreNetwork # noqa
 from pyfibre.model.multi_image.base_multi_image import BaseMultiImage # noqa
