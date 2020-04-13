86c95e18aa86a04689534dd7f1fed4ce923f59f4
franklongford <franklongford@gmail.com>
2020-04-13 18:46:21 +0100

    TST/FIX: fixes and update unit tests for CLI app

--- pyfibre/model/tests/test_iterator.py
+++ pyfibre/model/tests/test_iterator.py
@@ -14,15 +14,17 @@ class TestIterator(TestCase):
 
     def test_assign_images(self):
 
-        filenames = assign_images(self.image_dictionary)
+        filenames, image_type = assign_images(self.image_dictionary)
         self.assertEqual(1, len(filenames))
+        self.assertEqual('SHG-PL-Trans', image_type)
         self.assertEqual(
             '/directory/prefix-pl-shg-test.tif',
             filenames[0])
 
         self.image_dictionary.pop('SHG-PL-Trans')
-        filenames = assign_images(self.image_dictionary)
+        filenames, image_type = assign_images(self.image_dictionary)
         self.assertEqual(2, len(filenames))
+        self.assertEqual('SHG-PL-Trans', image_type)
         self.assertEqual(
             '/directory/prefix-shg-test.tif',
             filenames[0])
