eea0732b363ac5fb103db1a974b9a5fde29f2c5b
franklongford <franklongford@gmail.com>
2020-05-03 16:27:33 +0100

    CLN: add python.api to flake8

--- pyfibre/api.py
+++ pyfibre/api.py
@@ -1,17 +1,17 @@
-from pyfibre.model.objects.abc_pyfibre_object import ABCPyFibreObject # noqa
-from pyfibre.model.objects.base_graph import BaseGraph # noqa
-from pyfibre.model.objects.base_segment import BaseSegment # noqa
-from pyfibre.model.objects.base_graph_segment import BaseGraphSegment # noqa
-from pyfibre.model.objects.fibre import Fibre # noqa
-from pyfibre.model.objects.segments import CellSegment # noqa
-from pyfibre.model.objects.fibre_network import FibreNetwork # noqa
+from pyfibre.model.objects.abc_pyfibre_object import ABCPyFibreObject
+from pyfibre.model.objects.base_graph import BaseGraph
+from pyfibre.model.objects.base_segment import BaseSegment
+from pyfibre.model.objects.base_graph_segment import BaseGraphSegment
+from pyfibre.model.objects.fibre import Fibre
+from pyfibre.model.objects.segments import CellSegment
+from pyfibre.model.objects.fibre_network import FibreNetwork
 
-from pyfibre.model.multi_image.base_multi_image import BaseMultiImage # noqa
-from pyfibre.model.multi_image.multi_images import SHGImage, SHGPLTransImage # noqa
+from pyfibre.model.multi_image.base_multi_image import BaseMultiImage
+from pyfibre.model.multi_image.multi_images import SHGImage, SHGPLTransImage
 
-from pyfibre.model.tools.bd_cluster import BDFilter # noqa
-from pyfibre.model.tools.fibre_assigner import FibreAssigner # noqa
-from pyfibre.model.tools.fire_algorithm import FIREAlgorithm # noqa
+from pyfibre.model.tools.bd_cluster import BDFilter
+from pyfibre.model.tools.fibre_assigner import FibreAssigner
+from pyfibre.model.tools.fire_algorithm import FIREAlgorithm
 
-from pyfibre.model.image_analyser import ImageAnalyser # noqa
-from pyfibre.model.pyfibre_workflow import PyFibreWorkflow # noqa
+from pyfibre.model.image_analyser import ImageAnalyser
+from pyfibre.model.pyfibre_workflow import PyFibreWorkflow
