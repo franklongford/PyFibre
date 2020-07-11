e950e19582cd7ba159adbdfc5cb1ea4bc5272149
franklongford <franklongford@gmail.com>
2020-07-11 17:08:39 +0100

    CI: echo more for debug windows cmd script

--- .github/workflows/install-edm-windows.cmd
+++ .github/workflows/install-edm-windows.cmd
@@ -9,10 +9,12 @@ FOR /F "tokens=1,2,3 delims=." %%a in ("%EDM_FULL%") do (
 SET EDM_MAJOR_MINOR=%MAJOR%.%MINOR%
 SET EDM_PACKAGE=edm_cli_%EDM_FULL%_x86_64.msi
 SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
-SET COMMAND="(new-object net.webclient).DownloadFile('https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%', '%EDM_INSTALLER_PATH%')"
+SET EDM_URL='https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%'
+SET COMMAND="(new-object net.webclient).DownloadFile('%EDM_URL%', '%EDM_INSTALLER_PATH%')"
 
 ECHO %EDM_PACKAGE%
 ECHO %EDM_INSTALLER_PATH%
+ECHO %EDM_URL%
 
 IF NOT EXIST %EDM_INSTALLER_PATH% CALL powershell.exe -Command %COMMAND% || GOTO error
 CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR=C:\Enthought\edm || GOTO error
