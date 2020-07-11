ab28f9a5f17ede17847dcc3373626c205b2842a6
franklongford <franklongford@gmail.com>
2020-07-11 17:02:00 +0100

    CI: add echo dubugging

--- .github/workflows/install-edm-windows.cmd
+++ .github/workflows/install-edm-windows.cmd
@@ -1,5 +1,3 @@
-@ECHO OFF
-
 SETLOCAL EnableDelayedExpansion
 
 FOR /F "tokens=1,2,3 delims=." %%a in ("%EDM_FULL%") do (
@@ -12,6 +10,9 @@ SET EDM_MAJOR_MINOR=%MAJOR%.%MINOR%
 SET EDM_PACKAGE=edm_cli_%INSTALL_EDM_VERSION%_x86_64.msi
 SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
 SET COMMAND="(new-object net.webclient).DownloadFile('https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%', '%EDM_INSTALLER_PATH%')"
+
+ECHO %EDM_PACKAGE%
+ECHO %EDM_INSTALLER_PATH%
 
 IF NOT EXIST %EDM_INSTALLER_PATH% CALL powershell.exe -Command %COMMAND% || GOTO error
 CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR=C:\Enthought\edm || GOTO error
