65627a6c2487ceced0ff683843068f2a1db31f22
franklongford <franklongford@gmail.com>
2020-07-11 17:56:46 +0100

    CI: update environment paths

--- .github/workflows/install-edm-windows.cmd
+++ .github/workflows/install-edm-windows.cmd
@@ -12,10 +12,6 @@ SET EDM_INSTALLER_PATH=%HOMEDRIVE%%HOMEPATH%\.cache\%EDM_PACKAGE%
 SET EDM_URL=https://package-data.enthought.com/edm/win_x86_64/%EDM_MAJOR_MINOR%/%EDM_PACKAGE%
 SET COMMAND="(new-object net.webclient).DownloadFile('%EDM_URL%', '%EDM_INSTALLER_PATH%')"
 
-ECHO %EDM_PACKAGE%
-ECHO %EDM_INSTALLER_PATH%
-ECHO %EDM_URL%
-
 IF NOT EXIST %EDM_INSTALLER_PATH% CALL powershell.exe -Command %COMMAND% || GOTO error
 CALL msiexec /qn /i %EDM_INSTALLER_PATH% EDMAPPDIR="C:\Program Files\edm" || GOTO error
 
