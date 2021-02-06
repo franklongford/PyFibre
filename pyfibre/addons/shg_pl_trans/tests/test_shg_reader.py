FIX: Remove normalisation from image preprocessing (#70)
* FIX: do not normalise image intensity during clip_intensities function

* FIX: do not normalise image intensity loading of images

* FIX: use PL image for CellSegments in shg_pl_trans_segmentation

* FIX: normalise each figure image to 256

pyfibre/addons/shg_pl_trans/tests/test_shg_reader.py
@@ -89,11 +89,14 @@ class TestSHGReader(PyFibreTestCase):
 
     def test__format_image(self):
 
+        expected_2D_array = np.ones((10, 10)) * 2
+        expected_3D_array = np.ones((10, 10, 3)) * 2
+
         image = self.reader._format_image(
             np.ones((10, 10)) * 2
         )
         self.assertArrayAlmostEqual(
-            np.ones((10, 10)),
+            expected_2D_array,
             image
         )
         self.assertEqual(np.float, image.dtype)
@@ -102,7 +105,7 @@ class TestSHGReader(PyFibreTestCase):
             np.ones((10, 10, 3)) * 2
         )
         self.assertArrayAlmostEqual(
-            np.ones((10, 10, 3)),
+            expected_3D_array,
             image
         )
 
@@ -110,7 +113,7 @@ class TestSHGReader(PyFibreTestCase):
             np.ones((10, 10, 3)) * 2, 2
         )
         self.assertArrayAlmostEqual(
-            np.ones((10, 10)),
+            expected_2D_array,
             image
         )
 
@@ -120,8 +123,11 @@ class TestSHGReader(PyFibreTestCase):
         image = self.reader._format_image(
             test_image, 1
         )
+        expected_array = np.ones((3, 10, 10))
+        expected_array[0] *= 2
+        expected_array[2] *= 3
         self.assertArrayAlmostEqual(
-            np.ones((3, 10, 10)),
+            expected_array,
             image
         )
 

pyfibre/model/tools/tests/test_figures.py
@@ -65,13 +65,13 @@ class TestFigures(PyFibreTestCase):
         self.assertEqual(
             (10, 10, 3), segment_image.shape)
         self.assertTrue(
-            np.allclose(segment_image[..., 0][indices], 0.0075)
+            np.allclose(segment_image[..., 0][indices], 1.92)
         )
         self.assertTrue(
-            np.allclose(segment_image[..., 1][indices], 0.0075)
+            np.allclose(segment_image[..., 1][indices], 1.92)
         )
         self.assertTrue(
-            np.allclose(segment_image[..., 2][indices], 0.0075)
+            np.allclose(segment_image[..., 2][indices], 1.92)
         )
 
     def test_create_network_image(self):