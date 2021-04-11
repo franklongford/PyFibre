ec28bdb70c583517d634ca50f439c723dbf17ada
franklongford <franklongford@gmail.com>
2021-04-11 22:05:21 +0100

    FIX: Use raw intensity values in SHG-PL-Trans image segments (#80)
    * FIX: use raw data values in image segments
    
    * TST: include deterministic test for shg-pl-trans segmentation
    
    * TST: include regression test for shg-pl-trans segmentations to ensure raw intensity values are used

--- pyfibre/addons/shg_pl_trans/tools/tests/test_segmentation.py
+++ pyfibre/addons/shg_pl_trans/tools/tests/test_segmentation.py
@@ -1,29 +1,24 @@
-from skimage.io import imread
+from unittest import mock
+import numpy as np
 
-from pyfibre.tests.probe_classes.utilities import (
-    generate_image, generate_probe_graph)
 from pyfibre.tests.probe_classes.objects import ProbeFibreNetwork
 from pyfibre.tests.pyfibre_test_case import PyFibreTestCase
 from pyfibre.addons.shg_pl_trans.tests.probe_classes import (
     ProbeSHGPLTransImage)
 from pyfibre.addons.shg_pl_trans.tests.fixtures import (
-    test_shg_pl_trans_image_path)
+    test_fibre_mask)
 
-from ..segmentation import (
+from pyfibre.addons.shg_pl_trans.tools.segmentation import (
     shg_segmentation, shg_pl_trans_segmentation)
+
+
+MODULE_PATH = "pyfibre.addons.shg_pl_trans.tools.segmentation"
 
 
 class TestSegmentation(PyFibreTestCase):
 
     def setUp(self):
-        (self.image, self.labels,
-         self.binary, self.stack) = generate_image()
-        self.network = generate_probe_graph()
-
-        self.image_stack = imread(test_shg_pl_trans_image_path).mean(axis=-1)
-        for image in self.image_stack:
-            image /= image.max()
-
+        self.fibre_mask = np.where(np.sum(test_fibre_mask, axis=0), 1, 0)
         self.multi_image = ProbeSHGPLTransImage()
         self.fibre_networks = [ProbeFibreNetwork()]
 
@@ -36,8 +31,23 @@ class TestSegmentation(PyFibreTestCase):
         self.assertEqual(0, len(fibre_segments))
         self.assertEqual(1, len(cell_segments))
 
-    def test_shg_pl_trans_segmentation(self):
+        segment = cell_segments[0]
+        self.assertAlmostEqual(
+            120.6667, segment.region.intensity_image.max(), 4
+        )
 
-        shg_pl_trans_segmentation(
+    @mock.patch(f'{MODULE_PATH}.rgb_segmentation')
+    def test_shg_pl_trans_segmentation(self, mk_seg):
+        mk_seg.return_value = self.fibre_mask, ~self.fibre_mask
+        fibre_segments, cell_segments = shg_pl_trans_segmentation(
             self.multi_image, self.fibre_networks
         )
+        self.assertEqual(0, len(fibre_segments))
+        self.assertEqual(8, len(cell_segments))
+
+        max_values = [64.6667, 79.3333, 61.6667, 106.3333,
+                      89.6667, 116.0, 68.0, 101.3333]
+        for max_value, segment in zip(max_values, cell_segments):
+            self.assertAlmostEqual(
+                max_value, segment.region.intensity_image.max(), 4
+            )
