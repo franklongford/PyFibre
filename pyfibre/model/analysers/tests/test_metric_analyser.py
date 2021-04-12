9ad78841790f023ba8d444418968ebe918ecbd4d
franklongford <franklongford@gmail.com>
2021-04-13 00:12:14 +0100

    TST: additional unit testing

--- pyfibre/model/analysers/tests/test_metric_analyser.py
+++ pyfibre/model/analysers/tests/test_metric_analyser.py
@@ -17,32 +17,71 @@ class TestMetricAnalyser(TestCase):
 
     def setUp(self):
         self.metric_analyser = ProbeMetricAnalyser()
+        self.database = pd.DataFrame(
+            {'one': np.array([1., 2., 3., 4.]),
+             'two': np.array([4., 3., 2., 1.]),
+             'weights': np.array([1, 1, 2, 1])}
+        )
+        self.metrics = ['one', 'two']
 
     def test_metric_averaging(self):
-        database = pd.DataFrame(
-            {'one': np.array([1., 2., 3., 4.]),
-             'two': np.array([4., 3., 2., 1.])}
-        )
-        metrics = ['one']
-
         averages = metric_averaging(
-            database, metrics
+            self.database, self.metrics
         )
         self.assertAlmostEqual(2.5, averages['one'])
+        self.assertAlmostEqual(2.5, averages['two'])
 
         averages = metric_averaging(
-            database, metrics, weights=database['two'].values
+            self.database, self.metrics,
+            weights=self.database['weights'].tolist()
+        )
+        self.assertAlmostEqual(2.6, averages['one'])
+        self.assertAlmostEqual(2.4, averages['two'])
+
+        averages = metric_averaging(
+            self.database, self.metrics,
+            weights=self.database['weights'].values
+        )
+        self.assertAlmostEqual(2.6, averages['one'])
+        self.assertAlmostEqual(2.4, averages['two'])
+
+    def test_metric_averaging_zero_values(self):
+        self.database['one'] = np.array([1., 0, 3., 4.])
+        averages = metric_averaging(
+            self.database, self.metrics
         )
         self.assertAlmostEqual(2.0, averages['one'])
+        self.assertAlmostEqual(2.5, averages['two'])
 
-        database['one'][1] = None
+        self.database['two'] = np.array([4., 3., 0, 1.])
         averages = metric_averaging(
-            database, metrics
+            self.database, self.metrics,
+            weights=self.database['weights'].values
+        )
+        self.assertAlmostEqual(2.2, averages['one'])
+        self.assertAlmostEqual(1.6, averages['two'])
+
+    def test_metric_averaging_none_values(self):
+        self.database['one'] = np.array([1., None, 3., 4.])
+        averages = metric_averaging(
+            self.database, self.metrics
         )
         self.assertAlmostEqual(8/3, averages['one'])
+        self.assertAlmostEqual(2.5, averages['two'])
 
-        database['two'][2] = None
+        self.database['weights'] = np.array([1., 2., None, 1.])
         averages = metric_averaging(
-            database, metrics, weights=database['two'].values
+            self.database, self.metrics,
+            weights=self.database['weights'].values
         )
-        self.assertAlmostEqual(1.6, averages['one'])
+        self.assertAlmostEqual(2.5, averages['one'])
+        self.assertAlmostEqual(2.75, averages['two'])
+
+    def test_metric_averaging_error(self):
+        msg = ('Weights array must have same shape as '
+               'database columns')
+        with self.assertRaisesRegex(ValueError, msg):
+            metric_averaging(
+                self.database, self.metrics,
+                weights=np.array([1, 2])
+            )
