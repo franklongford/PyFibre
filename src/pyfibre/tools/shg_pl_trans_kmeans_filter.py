54837807a2390bb35ac9d90f55f66322fc608121
franklongford <franklongford@gmail.com>
2025-08-09 00:57:34 +0100

    DEV: include shg-pl-trans kmeans filter module

---
+++ src/pyfibre/tools/shg_pl_trans_kmeans_filter.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+from pyfibre.tools.base_kmeans_filter import BaseKmeansFilter
+
+
+def nonzero_mean(array):
+    """Return mean of non-zero values"""
+    return array.sum() / np.count_nonzero(array)
+
+
+def spherical_coords(coords):
+    """Transform cartesian coordinates to spherical
+
+    Parameters
+    ----------
+    coords: array_like, shape=(..., 3)
+        Values in 3D cartesian coordinates
+
+    Returns
+    -------
+    x, y, z: array-like, shape=(...)
+        Spherical coordinate system in 3D
+    """
+    x = np.arcsin(coords[..., 0])
+    y = np.arcsin(coords[..., 1])
+    z = np.arccos(coords[..., 2])
+
+    return x, y, z
+
+
+def distance_sum(vector, boundary):
+    boundary = np.array(boundary)
+
+    # Cost function, based on separation of all clusters
+    # from boundary
+    cost = np.nansum([np.abs(vec - boundary) for vec in vector])
+
+    return cost
+
+
+def binary_classifier_spherical(centres, intensities, boundary):
+    """Create new clusters from results of KMeans.
+    Attempts to add regularisation parameters
+
+    Parameters
+    ----------
+    centres: array-like, shape=(N, 4)
+        Centres of each cluster identified by K-means filter
+    intensities: array-like, shape=(N,)
+        Mean image intensity values for each centre
+    boundary: array-like, shape = (4,)
+        Minimum values for the boundary between classifier.
+
+    Returns
+    -------
+    clusters: array-like
+        Indices of centres in cellular regions
+    cost:
+        Cost associated with segmentation
+    """
+
+    boundary = np.array(boundary)
+
+    # Convert RGB centroids to spherical coordinates
+    x, y, z = spherical_coords(centres)
+
+    # Calculate mask and cost function associated with segmentation
+    # based on distance between boundary
+    vector = np.stack([x, y, z, intensities], axis=-1)
+
+    # Identify centroids in segmentation mask
+    mask = [all(vec <= boundary) for vec in vector]
+
+    cost = -distance_sum(vector, boundary)
+
+    return mask, cost
+
+
+class SHGPLTransKmeansFilter(BaseKmeansFilter):
+    def cellular_classifier(
+        self, label_image, centres, boundary=(0.65, 1.1, 1.40, 0.92)
+    ):
+        """Classifies centroids in image into either cellular or non-cellular
+        regions. This is based on boundary values"""
+
+        n_clusters = len(centres)
+
+        # Calculate average intensity of each labelled region
+        intensities = np.zeros(n_clusters)
+        for index in range(n_clusters):
+            indices = np.where(label_image == index)
+            cluster_values = self._greyscale[indices]
+            intensities[index] += nonzero_mean(cluster_values)
+
+        # Normalise centroids
+        magnitudes = np.sqrt(np.sum(centres**2, axis=-1))
+        magnitudes = np.repeat(magnitudes, centres.shape[-1])
+        centres = centres / magnitudes.reshape(centres.shape)
+
+        # Create cluster mask based on centre proximity to
+        # boundary
+        mask, cost = binary_classifier_spherical(centres, intensities, boundary)
+
+        return mask, cost
---
+++ src/pyfibre/tools/tests/test_shg_pl_trans_kmeans_filter.py
@@ -0,0 +1,115 @@
+import importlib.resources
+import numpy as np
+from skimage.io import imread
+from unittest import TestCase
+
+from pyfibre.tools.shg_pl_trans_kmeans_filter import (
+    nonzero_mean,
+    spherical_coords,
+    binary_classifier_spherical,
+    distance_sum,
+    SHGPLTransKmeansFilter,
+)
+
+
+class TestSHGPLTransKmeansFilter(TestCase):
+    def setUp(self):
+        testing_dir = self.enterContext(importlib.resources.path("pyfibre.testing"))
+        self.test_shg_pl_trans_image_path = str(
+            testing_dir / "fixtures" / "test-pyfibre-pl-shg-Stack.tif"
+        )
+
+        self.image_stack = imread(self.test_shg_pl_trans_image_path).mean(axis=-1)
+
+        self.image = np.zeros(self.image_stack[0].shape + (3,))
+        for index, image in enumerate(self.image_stack):
+            self.image[..., index] = image / image.max()
+
+        self.bd_filter = SHGPLTransKmeansFilter()
+
+    def test_nonzero_mean(self):
+        array = np.zeros(10)
+        array[3] = 2
+        array[7] = 4
+
+        self.assertEqual(3, nonzero_mean(array))
+
+    def test_distance_sum(self):
+        vector = np.ones((4, 4))
+        vector[0, 1] *= 3
+
+        cost = distance_sum(vector, (0, 1, 2, 3))
+
+        self.assertEqual(18.0, cost)
+
+    def test_spherical_coords(self):
+        array = np.linspace(0, 1, 10).repeat(3).reshape(10, 3)
+
+        x, y, z = spherical_coords(array)
+
+        np.testing.assert_almost_equal(
+            x,
+            np.array(
+                [
+                    0.0,
+                    0.11134101,
+                    0.22409309,
+                    0.33983691,
+                    0.46055399,
+                    0.58903097,
+                    0.72972766,
+                    0.89112251,
+                    1.09491408,
+                    1.57079633,
+                ]
+            ),
+        )
+        np.testing.assert_almost_equal(x, y)
+        np.testing.assert_almost_equal(
+            z,
+            np.array(
+                [
+                    1.57079633,
+                    1.45945531,
+                    1.34670323,
+                    1.23095942,
+                    1.11024234,
+                    0.98176536,
+                    0.84106867,
+                    0.67967382,
+                    0.47588225,
+                    0.0,
+                ]
+            ),
+        )
+
+    def test_binary_classifier_spherical(self):
+        centres = np.array([[0.1, 0.3, 0.9], [0.8, 0.3, 0.9], [0.1, 0.5, 0.9]])
+        intensities = np.arange(3)
+        param = (0.25, 0.5, 1.0, 2.0)
+
+        mask, cost = binary_classifier_spherical(centres, intensities, param)
+
+        np.testing.assert_almost_equal(np.array([1, 0, 0]), mask)
+        self.assertAlmostEqual(-6.038093407, cost)
+
+    def test_cellular_classifier(self):
+        self.bd_filter._greyscale = self.image[0]
+
+        mask, cost = self.bd_filter.cellular_classifier(
+            np.ones(self.image[0].shape), np.ones((3, 4))
+        )
+
+        np.testing.assert_almost_equal(np.array([False, True, False]), mask)
+        self.assertAlmostEqual(-3.6870816, cost)
+
+    def test_cluster_colours(self):
+        labels, centres = self.bd_filter._kmeans_cluster_colours(self.image)
+
+        np.testing.assert_almost_equal(np.arange(10), np.unique(labels))
+        self.assertEqual((10, 3), centres.shape)
+
+    def test_BD_filter(self):
+        mask_image = self.bd_filter.filter_image(self.image)
+
+        self.assertEqual((200, 200), mask_image.shape)
